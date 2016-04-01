a0eb2534f9d54caa3160076f76769e94634936ef
Carlos David Rios Vertel <carlos.rios@globallogic.com>
2016-04-01 09:30:39 -0300

    -- initial files.

---
+++ SwiftTraining/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTraining
+//
+//  Created by Carlos David Rios Vertel on 4/1/16.
+//  Copyright © 2016 Carlos Rios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
